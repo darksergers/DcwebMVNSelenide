734c425c1a641a698a95b59c37be2da0ad7223da
darksergers <darkserger@gmail.com>
2016-11-16 18:29:33 +0300

    отладка репортов 003

--- src/test/java/ru/dserg/autotest/page/Reports/RepairTimeReport.java
+++ src/test/java/ru/dserg/autotest/page/Reports/RepairTimeReport.java
@@ -13,6 +13,7 @@ import static com.codeborne.selenide.Selenide.$;
  * Created by Kalinin.S on 10.11.2016.
  */
 public class RepairTimeReport {
+    private int bad;
     public void selectStation(){
         $("#station-id").selectOptionByValue("11907059");
     }
@@ -24,12 +25,12 @@ public class RepairTimeReport {
         SelenideTable table = new SelenideTable($("#table-id").$(By.tagName("table")));
         label:for(int i = 1; i<$("#table-id").$$(By.tagName("tr")).size(); i=i+4){
             for(int j=1;i<=4;j++){
-                if (table.choiceTr(i,j).getText().length()>0){
+                if (Float.parseFloat(table.choiceTr(i,j).getText().replace(',','.'))>0){
                     continue label ;
                 }
             }
 
-
+            System.out.println("эта строка не ня "+i);
             return false;
     }
         return  true;
