2e15ecfe02322aa5baf6d8381d92e549c7fb3af4
miuli13 <Gorodilova.E@digdes.com>
2017-01-18 13:05:15 +0300

    "checkTable" added

--- src/test/java/ru/dserg/autotest/page/Reports/MediumTermRegimeControl.java
+++ src/test/java/ru/dserg/autotest/page/Reports/MediumTermRegimeControl.java
@@ -1,6 +1,7 @@
 package ru.dserg.autotest.page.Reports;
 
 import org.openqa.selenium.By;
+import ru.dserg.autotest.Utils.SelenideTable;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -19,6 +20,21 @@ public class MediumTermRegimeControl {
     public void clickShowTable(){
         $("#show-table").click();
     }
+    @Step("Проверка таблицы на наличие значений")
+    public boolean checkTable() throws Exception {
+        boolean b = false;
+        SelenideTable table = new SelenideTable($("#maket-table").$(By.tagName("tbody")));
+        label:for (int i=0;i<=$("#maket-table").$(By.tagName("tbody")).$$(By.tagName("tr")).size();i++) {
+            for (int j = 1; j <=$("#maket-table").$(By.tagName("tbody")).$(By.tagName("tr")).$$(By.tagName("td")).size(); j++) {
+                if(Float.parseFloat(table.choiceTr(i,j).getText().replace(',','.'))>0){
+                    b = true;
+                    break label;
 
+                }
 
+            }
+        }
+        if(b)return true;
+        else return false;
+    }
 }
